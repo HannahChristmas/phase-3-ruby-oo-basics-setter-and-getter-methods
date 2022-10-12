e8e0c9293ddc359ca2f0367910843ec0fd12ea44
HannahChristmas <hc109909@gmail.com>
2022-10-12 12:59:30 -0400

    commit tree 3a68d007c90da5450e508ca04fa80cc082110100

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
 class Person
+    def name=(name)
+        @name = name
+    end
 
+    def name
+        @name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    def job
+        @job
+    end
+
+    # attr_writer :name
+    # attr_accessor :price
+    # def initialize(name, job)
+    #     @name = name
+    #     @job = job
+    # end
 end
